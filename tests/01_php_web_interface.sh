d5d6d36bdce1df28cd855606357de4987c598625
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2017-12-11 18:24:04 +0100

    Test 01: expect Cloudy web interface on port 7000

--- tests/01_php_web_interface.sh
+++ tests/01_php_web_interface.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl http://localhost:7001 | grep -i cloudy | grep -i clommunity | grep -i netcommons && exit 0
+curl http://localhost:7000 | grep -i cloudy | grep -i clommunity | grep -i netcommons && exit 0
 exit 1
